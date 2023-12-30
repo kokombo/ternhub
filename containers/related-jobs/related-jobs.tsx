c41b81eb44027a3ee5b781b74121b4c67d864f33
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-12-30 15:57:01 +0100

    .

--- containers/related-jobs/related-jobs.tsx
+++ containers/related-jobs/related-jobs.tsx
@@ -10,10 +10,10 @@ const RelatedJobs = (props: Props) => {
   return (
     <>
       {props.relatedJobs && (
-        <section className="flex flex-col lg:gap-[42px] gap-[28px]">
+        <section className="flex flex-col lg:gap-[42px] gap-[28px] ">
           <div className="flex items-center justify-between">
             <h3
-              className="text-xl lg:text-[28px] font-[500] text-textblack"
+              className="text-xl lg:text-[28px] font-[500] text-textblack "
               style={Grotesk.style}
             >
               Related Jobs
